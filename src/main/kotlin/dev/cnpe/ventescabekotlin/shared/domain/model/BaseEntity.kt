2c0ed3d678e6d2c025b21e54265c40d2d3b179a4
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-18 15:23:25 -0400

    create BaseEntity

---
+++ src/main/kotlin/dev/cnpe/ventescabekotlin/shared/domain/model/BaseEntity.kt
@@ -0,0 +1,79 @@
+package dev.cnpe.ventescabekotlin.shared.domain.model
+
+import jakarta.persistence.*
+import org.hibernate.proxy.HibernateProxy
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.OffsetDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity(
+    @Id
+    @SequenceGenerator(
+        name = "primary_sequence",
+        sequenceName = "primary_sequence",
+        allocationSize = 1,
+        initialValue = 10000
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "primary_sequence"
+    )
+    @Column(name = "id")
+    open var id: Long? = null,
+
+    @Version
+    @Column(name = "version", nullable = false)
+    open var version: Int = 0
+
+) {
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    lateinit var createdAt: OffsetDateTime
+        protected set
+
+    @LastModifiedDate
+    @Column(name = "last_modified_at", nullable = false)
+    lateinit var lastModifiedAt: OffsetDateTime
+        protected set
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false)
+    lateinit var createdBy: String
+        protected set
+
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    var lastModifiedBy: String? = null
+        protected set
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null) return false
+
+        val otherEffectiveClass =
+            if (other is HibernateProxy) other.hibernateLazyInitializer.persistentClass else other.javaClass
+        val thisEffectiveClass =
+            if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass else this.javaClass
+
+        other as BaseEntity
+
+        return this.id != null && this.id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return if (id != null) {
+            id.hashCode()
+        } else {
+            System.identityHashCode(this)
+        }
+    }
+
+    override fun toString(): String {
+        return "${this.javaClass.simpleName}(id=$id)"
+    }
+}
