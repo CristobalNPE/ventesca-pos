41f36b0a22743661d744181ea2286790c770fb07
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-18 16:15:12 -0400

    feat(brands): Migrate BrandRepository interface to Kotlin

---
+++ src/main/kotlin/dev/cnpe/ventescabekotlin/brands/infrastructure/BrandRepository.kt
@@ -0,0 +1,21 @@
+package dev.cnpe.ventescabekotlin.brands.infrastructure
+
+import dev.cnpe.ventescabekotlin.brands.domain.model.Brand
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+
+interface BrandRepository : JpaRepository<Brand, Long> {
+
+    fun existsByName(name: String): Boolean
+
+    @Query(
+        """
+        select b.code.codeValue
+        from Brand b
+        where b.id = :brandId
+        """
+    )
+    fun betBrandCodeById(brandId: Long): String?
+
+    fun getBrandByIsDefaultTrue(): Brand?
+}
