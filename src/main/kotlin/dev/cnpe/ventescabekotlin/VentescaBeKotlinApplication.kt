ec311947c4f7cf06896f8d398363be82476d64dc
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-23 18:45:12 -0400

    Enable configuration properties for data source settings.

--- src/main/kotlin/dev/cnpe/ventescabekotlin/VentescaBeKotlinApplication.kt
+++ src/main/kotlin/dev/cnpe/ventescabekotlin/VentescaBeKotlinApplication.kt
@@ -1,9 +1,13 @@
 package dev.cnpe.ventescabekotlin
 
+import dev.cnpe.ventescabekotlin.tenant.config.MasterDataSourceProperties
+import dev.cnpe.ventescabekotlin.tenant.config.TenantDataSourceProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(TenantDataSourceProperties::class, MasterDataSourceProperties::class)
 class VentescaBeKotlinApplication
 
 fun main(args: Array<String>) {
