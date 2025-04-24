2e1199d3b793fbe53ac4ea15a681b82042e55c48
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-24 18:13:09 -0400

    Add KeycloakAdminProperties to configuration properties

--- src/main/kotlin/dev/cnpe/ventescabekotlin/VentescaBeKotlinApplication.kt
+++ src/main/kotlin/dev/cnpe/ventescabekotlin/VentescaBeKotlinApplication.kt
@@ -1,5 +1,6 @@
 package dev.cnpe.ventescabekotlin
 
+import dev.cnpe.ventescabekotlin.security.config.KeycloakAdminProperties
 import dev.cnpe.ventescabekotlin.tenant.config.MasterDataSourceProperties
 import dev.cnpe.ventescabekotlin.tenant.config.TenantDataSourceProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -7,7 +8,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableConfigurationProperties(TenantDataSourceProperties::class, MasterDataSourceProperties::class)
+@EnableConfigurationProperties(
+    TenantDataSourceProperties::class,
+    MasterDataSourceProperties::class,
+    KeycloakAdminProperties::class
+
+)
 class VentescaBeKotlinApplication
 
 fun main(args: Array<String>) {
