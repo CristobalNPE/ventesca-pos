d4ca09feb900bd1d8b2f27d83e6aad41138295ee
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-24 22:48:28 -0400

    Add DTOs for business responses and enhance requests with validation

--- src/main/kotlin/dev/cnpe/ventescabekotlin/business/application/dto/request/CreateBusinessBranchRequest.kt
+++ src/main/kotlin/dev/cnpe/ventescabekotlin/business/application/dto/request/CreateBusinessBranchRequest.kt
@@ -2,6 +2,7 @@ package dev.cnpe.ventescabekotlin.business.application.dto.request
 
 //TODO: We need to add validation and Schemas to this dto
 data class CreateBusinessBranchRequest(
+    val managerId: String?,
     val branchName: String?,
     val addressStreet: String?,
     val addressCity: String?,
