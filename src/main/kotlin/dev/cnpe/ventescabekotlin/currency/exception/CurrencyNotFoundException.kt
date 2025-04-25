73d18f24fe72c28ee25e7d0210a1f02490fd6cab
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-25 00:18:40 -0400

    feat: add CurrencyNotFoundException for unsupported or inactive currencies

---
+++ src/main/kotlin/dev/cnpe/ventescabekotlin/currency/exception/CurrencyNotFoundException.kt
@@ -0,0 +1,9 @@
+package dev.cnpe.ventescabekotlin.currency.exception
+
+/**
+ * Exception thrown when an operation requires a currency that is not
+ * supported, not active, or cannot be found in the system.
+ */
+class CurrencyNotFoundException(
+    val currencyCode: String
+) : RuntimeException("Currency not supported or not active: $currencyCode")
