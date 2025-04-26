035208bd8d08dcecf03650607c13456a09c2f232
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-26 13:34:29 -0400

    refactor(dto): replace String with StockUnitType for unitOfMeasure

--- src/main/kotlin/dev/cnpe/ventescabekotlin/catalog/application/dto/common/ProductStockData.kt
+++ src/main/kotlin/dev/cnpe/ventescabekotlin/catalog/application/dto/common/ProductStockData.kt
@@ -1,5 +1,6 @@
 package dev.cnpe.ventescabekotlin.catalog.application.dto.common
 
+import dev.cnpe.ventescabekotlin.inventory.domain.enums.StockUnitType
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Stock information summary (provided by Inventory module).")
@@ -11,5 +12,5 @@ data class ProductStockData(
     val minimumQuantity: Double?,
 
     @Schema(description = "Unit of measure (e.g., UNIT, KG).")
-    val unitOfMeasure: String? // FIXME: Enum from Inventory?
+    val unitOfMeasure: StockUnitType
 )
