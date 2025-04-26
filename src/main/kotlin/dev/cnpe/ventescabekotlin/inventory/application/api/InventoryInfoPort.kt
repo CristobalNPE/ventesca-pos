c7cf0b51754065bd49b4e338dd6b765a188c6dec
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-26 13:34:05 -0400

    feat: add new service ports for supplier, inventory, brand, and category info

---
+++ src/main/kotlin/dev/cnpe/ventescabekotlin/inventory/application/api/InventoryInfoPort.kt
@@ -0,0 +1,15 @@
+package dev.cnpe.ventescabekotlin.inventory.application.api
+
+import dev.cnpe.ventescabekotlin.inventory.application.api.dto.InventoryItemInfo
+
+
+interface InventoryInfoPort {
+
+    /**
+     * Retrieves basic inventory/stock info for a given product ID.
+     *
+     * @param productId The ID of the product.
+     * @return InventoryItemInfo containing stock details, its assumed that every products has inventory initialized.
+     */
+    fun getInventoryItemInfo(productId: Long): InventoryItemInfo
+}
--- src/main/kotlin/dev/cnpe/ventescabekotlin/suppliers/application/api/SupplierInfoPort.kt
+++ src/main/kotlin/dev/cnpe/ventescabekotlin/suppliers/application/api/SupplierInfoPort.kt
@@ -12,4 +12,16 @@ interface SupplierInfoPort {
      * @throws DomainException (e.g., RESOURCE_NOT_FOUND) if no default supplier is configured or found.
      */
     fun getDefaultSupplierId(): Long
+
+
+    /**
+     * Retrieves the name of the supplier based on the given supplier ID.
+     *
+     * @param id The unique identifier of the supplier whose name is to be retrieved.
+     * @return The name of the supplier corresponding to the provided ID.
+     * @throws DomainException (e.g., RESOURCE_NOT_FOUND) if no supplier is found with the given ID.
+     */
+    fun getSupplierNameById(id: Long): String
+
+
 }
