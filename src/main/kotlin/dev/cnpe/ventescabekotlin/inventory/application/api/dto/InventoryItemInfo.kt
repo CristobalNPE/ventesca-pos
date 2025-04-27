bdc6adfd9b0516a2a69524eb5159c81253d2b5a9
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-26 20:26:04 -0400

    `refactor: remove redundant schema name in InventoryItemInfo`

--- src/main/kotlin/dev/cnpe/ventescabekotlin/inventory/application/api/dto/InventoryItemInfo.kt
+++ src/main/kotlin/dev/cnpe/ventescabekotlin/inventory/application/api/dto/InventoryItemInfo.kt
@@ -3,7 +3,7 @@ package dev.cnpe.ventescabekotlin.inventory.application.api.dto
 import dev.cnpe.ventescabekotlin.inventory.domain.enums.StockUnitType
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(name = "InventoryItemInfo", description = "Information about inventory item stock and measurements.")
+@Schema(description = "Information about inventory item stock and measurements.")
 data class InventoryItemInfo(
 
     @Schema(description = "Current stock quantity of the item.", example = "10.0")
