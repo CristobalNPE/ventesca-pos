a83d8503925e9d8a28d0b25b64aa359041506c49
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-27 00:35:58 -0400

    refactor: rename fields in ProductCreatedEvent (id -> productId, unit -> unitType)

--- src/main/kotlin/dev/cnpe/ventescabekotlin/catalog/event/ProductCreatedEvent.kt
+++ src/main/kotlin/dev/cnpe/ventescabekotlin/catalog/event/ProductCreatedEvent.kt
@@ -3,6 +3,6 @@ package dev.cnpe.ventescabekotlin.catalog.event
 import dev.cnpe.ventescabekotlin.inventory.domain.enums.StockUnitType
 
 data class ProductCreatedEvent(
-    val id: Long,
-    val unit: StockUnitType
+    val productId: Long,
+    val unitType: StockUnitType
 )
