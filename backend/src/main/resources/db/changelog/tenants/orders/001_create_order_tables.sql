65bf34300c40a78b04a725876073913a3cf505c0
CristobalNPE <cristobal.npe@gmail.com>
2025-05-02 16:36:35 -0400

    feat(orders): add order number sequence

--- backend/src/main/resources/db/changelog/tenants/orders/001_create_order_tables.sql
+++ backend/src/main/resources/db/changelog/tenants/orders/001_create_order_tables.sql
@@ -1,5 +1,13 @@
 -- liquibase formatted sql
 
+-- changeset cnpe:orders-001-create-order-number-sequence
+CREATE SEQUENCE order_number_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 -- changeset cnpe:1745892086874-2
 CREATE TABLE order_items
 (
