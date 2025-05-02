55580d5d31b4a039639878eb03a8f34c51c76347
CristobalNPE <cristobal.npe@gmail.com>
2025-05-02 12:58:03 -0400

    `feat(database): add business_user_branch_assignments table and related constraints`

--- backend/src/main/resources/db/changelog/master/business/001_create_business_tables.sql
+++ backend/src/main/resources/db/changelog/master/business/001_create_business_tables.sql
@@ -1,9 +1,6 @@
 -- liquibase formatted sql
 
--- changeset Cristóbal:1745616714662-1
-CREATE SEQUENCE IF NOT EXISTS primary_sequence START WITH 10000 INCREMENT BY 1;
-
--- changeset Cristóbal:1745616714662-2
+-- changeset cnpe:1745973171140-2
 CREATE TABLE business_branches
 (
     id                BIGINT                      NOT NULL,
@@ -24,21 +21,29 @@ CREATE TABLE business_branches
     CONSTRAINT pk_business_branches PRIMARY KEY (id)
 );
 
--- changeset Cristóbal:1745616714662-3
+-- changeset cnpe:1745973171140-3
 CREATE TABLE business_payment_methods
 (
     business_id    BIGINT       NOT NULL,
     payment_method VARCHAR(255) NOT NULL
 );
 
--- changeset Cristóbal:1745616714662-4
+-- changeset cnpe:1745973171140-4
 CREATE TABLE business_user_assigned_roles
 (
     business_user_id BIGINT       NOT NULL,
     role_name        VARCHAR(255) NOT NULL
 );
 
--- changeset Cristóbal:1745616714662-5
+-- changeset cnpe:1745973171140-5
+CREATE TABLE business_user_branch_assignments
+(
+    business_branch_id BIGINT NOT NULL,
+    business_user_id   BIGINT NOT NULL,
+    CONSTRAINT pk_business_user_branch_assignments PRIMARY KEY (business_branch_id, business_user_id)
+);
+
+-- changeset cnpe:1745973171140-6
 CREATE TABLE business_users
 (
     id               BIGINT                      NOT NULL,
@@ -54,7 +59,7 @@ CREATE TABLE business_users
     CONSTRAINT pk_business_users PRIMARY KEY (id)
 );
 
--- changeset Cristóbal:1745616714662-6
+-- changeset cnpe:1745973171140-7
 CREATE TABLE businesses
 (
     id                BIGINT                      NOT NULL,
@@ -79,30 +84,38 @@ CREATE TABLE businesses
     CONSTRAINT pk_businesses PRIMARY KEY (id)
 );
 
--- changeset Cristóbal:1745616714662-7
+-- changeset cnpe:1745973171140-8
 ALTER TABLE business_users
     ADD CONSTRAINT uc_business_users_idp_user UNIQUE (idp_user_id);
 
--- changeset Cristóbal:1745616714662-8
+-- changeset cnpe:1745973171140-9
 ALTER TABLE businesses
     ADD CONSTRAINT uc_businesses_tenant UNIQUE (tenant_id);
 
--- changeset Cristóbal:1745616714662-9
+-- changeset cnpe:1745973171140-10
 CREATE UNIQUE INDEX idx_business_user_idp_id ON business_users (idp_user_id);
 
--- changeset Cristóbal:1745616714662-10
+-- changeset cnpe:1745973171140-11
 ALTER TABLE business_branches
     ADD CONSTRAINT FK_BUSINESS_BRANCHES_ON_BUSINESS FOREIGN KEY (business_id) REFERENCES businesses (id);
 
--- changeset Cristóbal:1745616714662-11
+-- changeset cnpe:1745973171140-12
 ALTER TABLE business_users
     ADD CONSTRAINT FK_BUSINESS_USERS_ON_BUSINESS FOREIGN KEY (business_id) REFERENCES businesses (id);
 
--- changeset Cristóbal:1745616714662-12
+-- changeset cnpe:1745973171140-13
 ALTER TABLE business_payment_methods
     ADD CONSTRAINT fk_business_payment_methods_on_business FOREIGN KEY (business_id) REFERENCES businesses (id);
 
--- changeset Cristóbal:1745616714662-13
+-- changeset cnpe:1745973171140-14
 ALTER TABLE business_user_assigned_roles
     ADD CONSTRAINT fk_business_user_assigned_roles_on_business_user FOREIGN KEY (business_user_id) REFERENCES business_users (id);
+
+-- changeset cnpe:1745973171140-15
+ALTER TABLE business_user_branch_assignments
+    ADD CONSTRAINT fk_bususebraass_on_business_branch FOREIGN KEY (business_branch_id) REFERENCES business_branches (id);
+
+-- changeset cnpe:1745973171140-16
+ALTER TABLE business_user_branch_assignments
+    ADD CONSTRAINT fk_bususebraass_on_business_user FOREIGN KEY (business_user_id) REFERENCES business_users (id);
 
